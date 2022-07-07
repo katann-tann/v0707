acf2d18f47188a4ab27742d1b6b5ff131eaa1ff8
Sasaki <104120192+katann-tann@users.noreply.github.com>
2022-07-07 13:59:11 +0900

    a

--- v0707/Form1.cs
+++ v0707/Form1.cs
@@ -30,14 +30,14 @@ namespace v0707
             data[2] = rand.Next(10);
             data[2] = rand.Next(10);
 
-            MessageBox.Show($"初期化数値.data[0]||{data[0]}||data[1]||{data[1]}||data[2]||{data[2] }||");
+            MessageBox.Show($"初期化数値\tdata[0]:{data[0]}\tdata[1]:{data[1]}\tdata[2]:{data[2] }");
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
             gk = 0;
             gk = data[0] + data[1] + data[2];
-            MessageBox.Show($"合計{gk }");
+            MessageBox.Show($"合計{gk }です");
         }
 
         private void button3_Click(object sender, EventArgs e)
@@ -46,7 +46,7 @@ namespace v0707
             hk = 0;
             gk = data[0] + data[1] + data[2];
             hk = gk / 3;
-            MessageBox.Show($"平均{hk}");
+            MessageBox.Show($"平均{hk}です");
         }
     }
 }
