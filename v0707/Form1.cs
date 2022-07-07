1c4e6a45ebbd22d1bb67a01e530d9ecc3cd137a4
Sasaki <104120192+katann-tann@users.noreply.github.com>
2022-07-07 13:52:22 +0900

    復習問題終了!

--- v0707/Form1.cs
+++ v0707/Form1.cs
@@ -12,10 +12,41 @@ namespace v0707
 {
     public partial class Form1 : Form
     {
+        int[] data = new int[3];
+        int gk, hk;
+        static Random rand = new Random();
         public Form1()
         {
             InitializeComponent();
         }
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //★のランダム化
+            data[0] = rand.Next(10);
+            data[0] = rand.Next(10);
+            data[1] = rand.Next(101);
+            data[1] = rand.Next(10);
+            data[2] = rand.Next(10);
+            data[2] = rand.Next(10);
+
+            MessageBox.Show($"初期化数値.data[0]||{data[0]}||data[1]||{data[1]}||data[2]||{data[2] }||");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            gk = 0;
+            gk = data[0] + data[1] + data[2];
+            MessageBox.Show($"合計{gk }");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            gk = 0;
+            hk = 0;
+            gk = data[0] + data[1] + data[2];
+            hk = gk / 3;
+            MessageBox.Show($"平均{hk}");
+        }
     }
 }
